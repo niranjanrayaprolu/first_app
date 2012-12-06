41b26de56ec9cbdfb99a3ebc5a30da564d7bea53
Slashroot <slashroot@slashroot-ThinkCentre-M70e.(none)>
2012-12-06 10:32:00 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '007a676665dbc7714129ce7e4a852833f32b781b0d5d58b89057b2489f2851d72662d57b5768bf3f4d6bbe81cc7829375b44cf224dedd6ace06a05f44ea02102'
